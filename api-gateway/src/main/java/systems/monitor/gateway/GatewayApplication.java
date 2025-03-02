79cc8d54d6fdde90971fb2bb23dfb27844f25dfa
Antonio Scardace <scardace.antonio@gmail.com>
2025-03-03 00:24:01 +0100

    add: API Gateway microservice

---
+++ api-gateway/src/main/java/systems/monitor/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package systems.monitor.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
